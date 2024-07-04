fce6ad8d673f6a83b217659d3e72f28176985adf
MohamedAazil <142242204+MohamedAazil@users.noreply.github.com>
2024-07-04 22:35:49 +0530

    Add files via upload

---
+++ portfolio_website.py
@@ -0,0 +1,52 @@
+import streamlit as st
+import google.generativeai as genai
+api_key = st.secrets["GOOGLE_API_KEY"]
+genai.configure(api_key=api_key)
+model = genai.GenerativeModel('gemini-1.5-flash')
+
+persona = """ 
+    You are Murtaza AI bot. You help people answer questions about your self (i.e Murtaza) Answer as if you are responding . dont answer in second or third person.
+    If you don't know they answer you simply say "That's a secret"
+    Here is more info about Murtaza:
+"""
+
+
+col1,col2 = st.columns(2)
+with col1:
+    st.subheader("Hello!!:wink::wave:")
+    st.title("I am Mohamed Aazil")
+    #st.page_link(label="LinkedIn",url="https://www.linkedin.com/in/mohamed-aazil/")
+    if st.button("LinkedIn"):
+        st.write("[Go to LinkedIn](https://www.linkedin.com/in/mohamed-aazil/)")
+    
+with col2:
+    st.image("Images/Aazil.jpg")
+
+st.title("")
+
+st.title("Aazil's AI Navigator")
+st.subheader("I'm here to guide you")
+qn = st.text_input("Ask anything about me")
+
+
+if st.button("Ask",use_container_width=10):
+    st.header(model.generate_content(persona + "Here is the question" + qn).text)
+
+st.title("")
+col1,col2 = st.columns(2)
+
+st.title("Projects")
+col1,col2 = st.columns(2)
+with col1:
+    st.subheader("- Drowsiness Detection")
+    st.subheader("- Chatbot Using Langchain")
+    st.subheader('- "Linkberry" Course Cohort')
+    st.subheader("- Whatsapp Sentiment Analysis")
+    st.subheader("- Rock Paper Scissors Game")
+with col2:
+    if st.button("Github"):
+        st.write("https://github.com/MohamedAazil/Projects/tree/main/Projects/Python/Drowsiness_detection")
+
+st.title("Academics")
+st.subheader("3rd Year")
+st.slider("",1,4,3)
